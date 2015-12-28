a76c77f43ca171fd858ec7f8d78e2a7607b3c0fa
Kasie Jun <jeoncas@gmail.com>
2015-12-28 21:43:15 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Plotbunnyshare::Application.config.secret_key_base = '8b86d6f328cc5fafd76d00167cd62f10f9fd7ee920cb46d8a17fb6f181e858f0fb9964ce4112b3c508e7ad29c7ae4410cf1506063820eaed366e1fc01cd23f13'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Plotbunnyshare::Application.config.session_store :cookie_store, key: '_plotbunnyshare_session'
