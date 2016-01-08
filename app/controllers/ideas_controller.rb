08ea5db602b2be4dc76528f0f21fee3cb4f05714
Kasie Jun <jeoncas@gmail.com>
2016-01-08 00:27:04 +0000

    Ready

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -20,6 +20,12 @@ class IdeasController < ApplicationController
 		@idea = Idea.find(params[:id])
 	end
 
+	def update
+		@idea = Idea.find(params[:id])
+		@idea.update_attributes(idea_params)
+		redirect_to root_path
+	end
+
 	private
 
 	def idea_params
