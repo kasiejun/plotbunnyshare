daa5289d82f7ff3129f08bc73051c6969e9604a6
Kasie Jun <jeoncas@gmail.com>
2016-01-19 00:25:35 +0000

    Ready

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Plotbunnyshare::Application.configure do
+  config.action_mailer.default_url_options = { host: 'plotbunnyshare.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
